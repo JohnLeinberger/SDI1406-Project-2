a35238431a3e7dce6df93b1f7c7908f1979b14d0
John Leinberger <jleinberger@fullsail.edu>
2014-06-09 22:25:07 -0400

    Added boolean function to code and corrected syntax errors.

--- Leinberger_John_Assignment2.js
+++ Leinberger_John_Assignment2.js
@@ -8,18 +8,61 @@ var canOfSoup;
 var cannedMeat;
 var waterCount;
 var cookTime;
-var continueHiking = false;
+var timeToCook = false;
+var singASong = false;
 var direction;
+var distanceHiked = 0;
 
-//prompt for procedure
-direction = prompt("What direction would you like to head, North or South?", "Enter North or South");
+
 
 //procedure for directional heading
-directionProcedure = function (direction){
+function directionProcedure(direction){
+    
     if(direction === "North"){
         console.log("To the North the road is pretty empty, you can see anyone or anything coming from miles in either direction.");
     }else{
         console.log("To the South you see what appears to be some kind of beast roaming the forest, he doesn't seem to have noticed you.");
     }
     console.log("You decide it's in your best interest to continue North, putting more distance between you and the beast.");
+
 };
+
+//boolean function with two arguments
+function travelDistance(distanceHiked, singASong) {
+    
+    while(distanceHiked > 0 && (singASong === true || singASong === false)){
+        
+        distanceHiked--;
+        console.log("You have " + distanceHiked + " miles to go, until you can rest.");
+        
+        if (singASong === true) {
+            
+            console.log("You sing a new song during mile " + distanceHiked + ".");
+            
+        }else{
+            
+            console.log("Your mind aimlessly wanders about all the different things hiding in the woods...");
+            
+        }
+    }
+    
+    return true;
+};
+
+
+
+
+//main code
+
+//prompt for procedure
+direction = prompt("What direction would you like to head, North or South?", "Enter North or South");
+
+//call to directionProcedure function
+directionProcedure(direction);
+
+//arguments for boolean function
+distanceHiked = parseInt(prompt("Enter the number of miles you want to hike today: ", "Enter the number of miles."));
+singASong = confirm("Do you want to sing a song while you hike?");
+
+//call to travelDistance function
+timeToCook = travelDistance(distanceHiked, singASong);
